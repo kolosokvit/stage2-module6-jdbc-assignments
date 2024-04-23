29aeebce63ccf70cd3cd8ca56fb39f0026ffac0e
Vitali Kalaskou <kolosokvit@gmail.com>
2024-04-24 00:02:49 +0300

    Implement some functionality

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -5,6 +5,15 @@ import javax.sql.DataSource;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
 @Getter
 @Setter
 public class CustomDataSource implements DataSource {
@@ -15,11 +24,73 @@ public class CustomDataSource implements DataSource {
     private final String password;
 
     private CustomDataSource(String driver, String url, String password, String name) {
+        this.driver = driver;
+        this.url = url;
+        this.password = password;
+        this.name = name;
     }
 
     public static CustomDataSource getInstance() {
-
+        if (instance == null) {
+            Properties properties = new Properties();
+            try (FileReader fileReader = new FileReader("/home/vitali/projects/mjc-school/stage2/stage2-module6-jdbc-assignments/src/main/resources/app.properties")) {
+                properties.load(fileReader);
+                String driver = properties.getProperty("postgres.driver");
+                String url = properties.getProperty("postgres.url");
+                String password = properties.getProperty("postgres.password");
+                String name = properties.getProperty("postgres.name");
+                instance = new CustomDataSource(driver, url, password, name);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         return instance;
     }
 
+    @Override
+    public Connection getConnection() throws SQLException {
+        return new CustomConnector().getConnection(url, name, password);
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return new CustomConnector().getConnection(url, username, password);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        throw new UnsupportedOperationException("Unsupported method 'getLogWriter()'");
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        throw new UnsupportedOperationException("Unsupported method 'setLogWriter()'");
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        throw new UnsupportedOperationException("Unsupported method 'setLoginTimeout()'");
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        throw new UnsupportedOperationException("Unsupported method 'getLoginTimeout()'");
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        throw new UnsupportedOperationException("Unsupported method 'getParentLogger()'");
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        throw new UnsupportedOperationException("Unsupported method 'unwrap()'");
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        throw new UnsupportedOperationException("Unsupported method 'isWrapperFor()'");
+    }
 }
+
+
--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -6,9 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -21,28 +20,91 @@ public class SimpleJDBCRepository {
     private PreparedStatement ps = null;
     private Statement st = null;
 
-    private static final String createUserSQL = "";
-    private static final String updateUserSQL = "";
-    private static final String deleteUser = "";
-    private static final String findUserByIdSQL = "";
-    private static final String findUserByNameSQL = "";
-    private static final String findAllUserSQL = "";
+    private static final String createUserSQL = "INSERT INTO myusers (firstname, lastname, age) VALUES (?, ?, ?)";
+    private static final String updateUserSQL = "UPDATE myusers SET firstname = ?, lastname = ?, age = ? WHERE id = ?";
+    private static final String deleteUser = "DELETE FROM myusers WHERE id = ?";
+    private static final String findUserByIdSQL = "SELECT * FROM myusers WHERE id = ?";
+    private static final String findUserByNameSQL = "SELECT * FROM myusers WHERE firstname = ?";
+    private static final String findAllUserSQL = "SELECT * FROM myusers";
 
     public Long createUser() {
+        throw new UnsupportedOperationException("method isn't implemented");
     }
 
-    public User findUserById(Long userId) {
+    public User findUserById(Long userId) throws SQLException {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+            ps = connection.prepareStatement(findUserByIdSQL);
+            ps.setLong(1, userId);
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setFirstName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getInt(4));
+                return user;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        throw new SQLException("Can't find user with id = " + userId);
     }
 
-    public User findUserByName(String userName) {
+    public User findUserByName(String userName) throws SQLException {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+            ps = connection.prepareStatement(findUserByNameSQL);
+            ps.setString(1, userName);
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setFirstName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getInt(4));
+                return user;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        throw new SQLException("Can't find user with name = " + userName);
     }
 
     public List<User> findAllUser() {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+            st = connection.createStatement();
+            ResultSet resultSet = st.executeQuery(findAllUserSQL);
+            List<User> allUsers = new ArrayList<>();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setFirstName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge(resultSet.getInt(4));
+                allUsers.add(user);
+            }
+            return allUsers;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public User updateUser() {
+        throw new UnsupportedOperationException("method isn't implemented");
     }
 
-    private void deleteUser(Long userId) {
+    public void deleteUser(Long userId) {
+        try {
+            connection = CustomDataSource.getInstance().getConnection();
+            ps = connection.prepareStatement(deleteUser);
+            ps.setLong(1, userId);
+            ps.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
+
